4cbbfb4ed1bd0f432ebe2e68bec1faa29833328a
Sergio Murilo <142538476+Goosyx@users.noreply.github.com>
2025-02-23 19:13:22 -0300

    Delete servr-back directory

--- servr-back/src/errors/HttpError.ts
+++
@@ -1,9 +0,0 @@
-export enum HttpError {
-    BAD_REQUEST = 400,
-    UNAUTHORIZED = 401,
-    PAYMENT_REQUIRED = 402,
-    FORBIDDEN = 403,
-    NOT_FOUND = 404,
-    INTERNAL_SERVER_ERROR = 500,
-    NOT_IMPLENTED = 401
-}
